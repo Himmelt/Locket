992754b158a894052f7eb7242d95fc91e726912b
Himmelt <master@void-3.cn>
2020-01-10 00:47:30 +0800

    add mini jar library fix #16

--- SpongeLocket/src/main/java/me/ryanhamshire/griefprevention/api/GriefPreventionApi.java
+++
@@ -1,8 +0,0 @@
-package me.ryanhamshire.griefprevention.api;
-
-import me.ryanhamshire.griefprevention.api.claim.ClaimManager;
-import org.spongepowered.api.world.World;
-
-public interface GriefPreventionApi {
-    ClaimManager getClaimManager(World world);
-}
