2298a87fbef14f97750f73f012a18c8d64789e25
Himmelt <master@void-3.cn>
2020-01-10 00:51:08 +0800

    update

--- SpongeLocket/src/main/java/org/soraworld/locket/data/Result.java
+++ SpongeLocket/src/main/java/org/soraworld/locket/data/Result.java
@@ -1,12 +1,38 @@
 package org.soraworld.locket.data;
 
+/**
+ * The enum Result.
+ *
+ * @author Himmelt
+ */
 public enum Result {
+    /**
+     * Sign user result.
+     */
     SIGN_USER(false, true),
+    /**
+     * Sign owner result.
+     */
     SIGN_OWNER(true, true),
+    /**
+     * Locked result.
+     */
     LOCKED(false, false),
+    /**
+     * Not locked result.
+     */
     NOT_LOCKED(true, true),
+    /**
+     * Multi owners result.
+     */
     MULTI_OWNERS(false, false),
+    /**
+     * Multi blocks result.
+     */
     MULTI_BLOCKS(false, false),
+    /**
+     * Other protect result.
+     */
     OTHER_PROTECT(false, false);
 
     private boolean edit, use;
@@ -16,10 +42,20 @@ public enum Result {
         this.use = use;
     }
 
+    /**
+     * Can edit boolean.
+     *
+     * @return the boolean
+     */
     public boolean canEdit() {
         return edit;
     }
 
+    /**
+     * Can use boolean.
+     *
+     * @return the boolean
+     */
     public boolean canUse() {
         return use;
     }
