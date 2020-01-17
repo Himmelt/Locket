b2c13221ed64f838d4d5d6994071e5d29a484573
Himmelt <master@void-3.cn>
2020-01-18 01:28:21 +0800

    custom tile-sign test pass update #26

--- SpigotLocket/src/main/java/org/soraworld/locket/Locket.java
+++ SpigotLocket/src/main/java/org/soraworld/locket/Locket.java
@@ -1,7 +1,12 @@
 package org.soraworld.locket;
 
+import net.minecraft.server.v1_7_R4.TileEntity;
 import org.soraworld.locket.manager.LocketManager;
+import org.soraworld.locket.nms.TileEntitySign;
 import org.soraworld.violet.plugin.SpigotPlugin;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
 
 /**
  * @author Himmelt
@@ -10,4 +15,20 @@ public final class Locket extends SpigotPlugin<LocketManager> {
     public static final String PLUGIN_ID = "locket";
     public static final String PLUGIN_NAME = "Locket";
     public static final String PLUGIN_VERSION = "1.2.3";
+
+    static {
+        try {
+            // TODO Optimize
+            Field field = TileEntity.class.getDeclaredField("i");
+            field.setAccessible(true);
+            HashMap map = (HashMap<String, Class<?>>) field.get(null);
+            map.put("Sign", TileEntitySign.class);
+            field = TileEntity.class.getDeclaredField("j");
+            field.setAccessible(true);
+            map = (HashMap<Class<?>, String>) field.get(null);
+            map.put(TileEntitySign.class, "Sign");
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
 }
--- SpigotLocket/src/main/java/org/soraworld/locket/nms/TileEntitySign.java
+++ SpigotLocket/src/main/java/org/soraworld/locket/nms/TileEntitySign.java
@@ -3,6 +3,7 @@ package org.soraworld.locket.nms;
 import net.minecraft.server.v1_7_R4.NBTTagCompound;
 import net.minecraft.server.v1_7_R4.Packet;
 import net.minecraft.server.v1_7_R4.PacketPlayOutUpdateSign;
+import org.soraworld.violet.util.ChatColor;
 
 /**
  * @author Himmelt
@@ -16,14 +17,23 @@ public class TileEntitySign extends net.minecraft.server.v1_7_R4.TileEntitySign 
         this.z = compound.getInt("z");
         for (int i = 0; i < 4; ++i) {
             this.lines[i] = compound.getString("Text" + (i + 1));
-            if (this.lines[i].length() > 15) {
-                this.lines[i] = this.lines[i].substring(0, 15);
-            }
         }
     }
 
     @Override
     public Packet getUpdatePacket() {
-        return new PacketPlayOutUpdateSign(this.x, this.y, this.z, this.lines);
+        return new PacketPlayOutUpdateSign(x, y, z, sendClientLines());
+    }
+
+    private String[] sendClientLines() {
+        String[] texts = new String[4];
+        for (int i = 0; i < 4; i++) {
+            texts[i] = lines[i].length() <= 15 ? lines[i] : lines[i].substring(0, 15);
+            int length = texts[i].length();
+            if (length >= 1 && texts[i].charAt(length - 1) == ChatColor.TRUE_COLOR_CHAR) {
+                texts[i] = texts[i].substring(0, length - 1);
+            }
+        }
+        return texts;
     }
 }
